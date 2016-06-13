d52b9c3aefc2580a299ff6cbd8853fdc864dd11b
Denise Bradley <porenn4u@outlook.com>
2016-06-12 22:19:17 -0400

    Initial Project Changes for S5A1

---
+++ BehaviorsDemoApp/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using BehaviorsDemoApp.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace BehaviorsDemoApp.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
